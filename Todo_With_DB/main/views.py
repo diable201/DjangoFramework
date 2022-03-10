41f51136b42562b53ce4de59f1e6adfb89b40afd
Seitbekov Sanzhar <diable201@protonmail.com>
2022-03-10 12:32:52 +0600

    Group title in html

--- Todo_With_DB/main/views.py
+++ Todo_With_DB/main/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
-from .models import Task
+from .models import Task, TodoList
 
 
 # Create your views here.
 def all_todos(request, group_id):
     context = {
         'todos': Task.objects.filter(group_id=group_id).order_by('title'),
+        'group': TodoList.objects.get(id=group_id)
     }
     return render(request, 'todos/all_tasks.html', context=context)
 
 
 def completed_todos(request, group_id):
     context = {
-        'todos': Task.objects.filter(group_id=group_id, done=True).order_by('title')
+        'todos': Task.objects.filter(group_id=group_id, done=True).order_by('title'),
+        'group': TodoList.objects.get(id=group_id)
     }
     return render(request, 'todos/completed_tasks.html', context=context)
