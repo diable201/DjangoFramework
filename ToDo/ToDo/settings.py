49c77c46b76c80876877c6465d6c076fc127686e
Seitbekov Sanzhar <diable201@protonmail.com>
2022-05-28 22:12:44 +0600

    Key changed

--- ToDo/ToDo/settings.py
+++ ToDo/ToDo/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^8#s@+*w0zglmprp2f*n#8p&%_kvt5i9n+ndwef-$*n4md=laz'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
